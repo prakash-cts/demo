2c22b21029ba7080a3a93acd89ff3e65d916992b
Prakash Kaliyannan <prakash.kaliyannan@cognizant.com>
2022-02-23 10:05:56 +0530

    Created h2 connection with flyway and hibernate

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/")
 public class DemoController {
+    @Autowired
+    StudentRepository repository;
+
+    @GetMapping
+    public String getStudent(){
+        save();
+        List<Student> s = repository.findAll();
+        return "Hello"+ s.toString();
+    }
+    private void save(){
+        Student s = new Student("prakash");
+        repository.save(s);
+    }
 }
---
+++ src/main/resources/db/migration/V1__1.sql
@@ -0,0 +1,6 @@
+create table Student (
+    id SERIAL primary key,
+    name varchar(255)
+);
+
+insert into Student(name) values('pk');
